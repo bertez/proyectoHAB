8f4f846b308dccdd3112451a6768120f4e73b1ba
victor <victormpl1983@gmail.com>
2020-12-15 23:12:17 +0100

    añadido usernav

---
+++ front/src/user/UserNav.js
@@ -0,0 +1,16 @@
+
+
+
+function UserNav(){
+
+    return(
+        <div className="user-nav">
+            <ul className="nav-list">
+                <li>mi perfil</li>
+                <li>crear experiencia</li>
+                <li>experiencias</li>
+            </ul>
+        </div>
+
+    );
+}
